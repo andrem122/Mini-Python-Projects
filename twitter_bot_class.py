3b10aee52b62f91645f55facc86c7e2334029aa1
Andre Mashraghi <andrem122@users.noreply.github.com>
2018-04-02 10:40:57 -0400

    Update twitter_bot_class.py

--- twitter_bot_class.py
+++ twitter_bot_class.py
@@ -1,5 +1,6 @@
 from time import sleep
 import tweepy as tp
+import os
 
 class Twitter_Bot:
 
@@ -38,3 +39,19 @@ class Twitter_Bot:
     #follow users by search query
     def follow_user(self, query, num_items):
         self.loop_through_tweets(query=query, num_items=num_items, action='follow')
+
+    #update status with scraped web images
+    def tweet_images(self):
+        #go to the directory with the images
+        os.chdir('twitter_content/twitter_images')
+
+        #get images in  current directory
+        images = os.listdir('.')
+
+        #tweet each image
+        for image in images:
+            try:
+                self.api.update_with_media(image)
+                sleep(10)
+            except tp.TweepError as e:
+                print(e.reason)
